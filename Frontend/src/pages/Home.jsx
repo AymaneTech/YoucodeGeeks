b19bdca0e08cd00a4ac7571b87ef0e7f45cdd6ef
geek <elmainiaymane03@gmail.com>
2024-03-24 08:52:22 +0000

    extract a main component / working on create questions / there is problmes in editor pakcage

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,54 +1,47 @@
-import Navbar from "../components/partials/Navbar";
-import Sidebar from "../components/partials/Sidebar";
-import { Tabs, TabList, TabPanels, Tab, TabPanel, TabIndicator, Button } from '@chakra-ui/react'
+import { Tabs, TabList, TabPanels, Tab, TabPanel, TabIndicator } from '@chakra-ui/react'
 import { Questions } from "../components/Questions";
 import { posts } from "../components/data/posts";
 import Posts from "../components/Posts";
+import { Slink } from "../components/partials/Buttons";
+import Model from "./Model";
 
 
 export const Home = () => {
   return (
-    <main className="bg-[#16161a] text-white grid grid-cols-12 grid-rows-auto font-mono">
-      <Navbar />
-      <div className="col-span-12 md:col-span-2 ">
-        <Sidebar />
-      </div>
-      <div className="col-span-12 md:col-span-10 mx-2 md:mx-20">
-        <Posts posts={posts} />
+    <Model>
+      < Posts posts={posts} />
 
-        <Tabs position="" variant="unstyled">
-          <div className="flex justify-between items-center p-3">
-            <TabList>
-              <Tab>
-                All Questions</Tab>
-              <Tab>Two</Tab>
-              <Tab>Three</Tab>
-            </TabList>
-            <div>
-              <Button>Ask a question</Button>
-            </div>
-          </div>
-          <TabIndicator
-            mt="-1.5px"
-            height="2px"
-            bg="blue.500"
-            borderRadius="1px"
-          />
-          <TabPanels>
-            <TabPanel>
-              <Questions posts={posts} />
-            </TabPanel>
-            <TabPanel>
-              <p>two!</p>
-            </TabPanel>
-            <TabPanel>
-              <p>three!</p>
-            </TabPanel>
-          </TabPanels>
-        </Tabs>
-      </div>
+      <Tabs position="" variant="unstyled">
+        <div className="flex justify-between items-center p-3">
+          <TabList>
+            <Tab>
+              All Questions</Tab>
+            <Tab>Two</Tab>
+            <Tab>Three</Tab>
+          </TabList>
+          <Slink to="questions/create">Ask A question</Slink>
 
-    </main >
+        </div>
+        <TabIndicator
+          mt="-1.5px"
+          height="2px"
+          bg="blue.500"
+          borderRadius="1px"
+        />
+        <TabPanels>
+          <TabPanel>
+            <Questions posts={posts} />
+          </TabPanel>
+          <TabPanel>
+            <p>two!</p>
+          </TabPanel>
+          <TabPanel>
+            <p>three!</p>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+    </Model>
+
 
   );
 };
---
+++ Frontend/src/pages/Questions/Create.jsx
@@ -0,0 +1,60 @@
+
+import Model from "../Model"
+import {
+  FormControl,
+  FormHelperText,
+  FormLabel,
+  Input,
+  ListItem,
+  Textarea,
+  UnorderedList,
+} from '@chakra-ui/react'
+
+
+
+export const Create = () => {
+  return (
+    <Model>
+      <section className="mt-12 flex flex-col gap-8">
+        <div className="flex items-center gap-8">
+          <div className="flex flex-col gap-8">
+            <h1 className="text-3xl">Ask public question to the community: </h1>
+            <div className="flex flex-col gap-4 p-6 border w-[800px]  border-[#7b7f93] rounded-lg  bg-[#1c1e27]">
+              <h2 className="font-bold text-3xl">Hey Nerd! </h2>
+              <p>Writing a good question let others understand and help you find the solution, so write a clever question</p>
+              <strong>Steps to follow: </strong>
+              <UnorderedList>
+                <ListItem>Lorem ipsum dolor sit amet</ListItem>
+                <ListItem>Consectetur adipiscing elit</ListItem>
+                <ListItem>Integer molestie lorem at massa</ListItem>
+                <ListItem>Facilisis in pretium nisl aliquet</ListItem>
+              </UnorderedList>
+            </div>
+          </div>
+          <div>
+            <img src="/src/assets/code.png" alt="" />
+          </div>
+        </div>
+        <div className="flex flex-col gap-4 p-6 border border-[#7b7f93] rounded-lg  bg-[#1c1e27]">
+          <form>
+            <div className="flex flex-col gap-6">
+              <FormControl isRequired>
+                <FormLabel>Question Title</FormLabel>
+                <Input type='text' placeholder="eg: How fetch an api in react ?" />
+                <FormHelperText>Be specific, and imagine you're asking a person.</FormHelperText>
+              </FormControl>
+
+
+              <FormControl isRequired>
+                <FormLabel>Question Details</FormLabel>
+                <Textarea placeholder="eg: I don't find the wanted data in the response" />
+                <FormHelperText>Expand what you said in the title.</FormHelperText>
+              </FormControl>
+            </div>
+          </form>
+        </div>
+      </section>
+    </Model>
+  )
+}
+
