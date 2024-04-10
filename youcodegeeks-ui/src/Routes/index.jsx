5c373f5adec470581d3f631121b2066183521c64
geek <elmainiaymane03@gmail.com>
2024-04-10 20:00:19 +0000

    set up the shad cn and a base layout for all the pages

--- youcodegeeks-ui/src/Routes/index.jsx
+++ youcodegeeks-ui/src/Routes/index.jsx
@@ -3,23 +3,28 @@ import {Login} from "../Pages/Login.jsx";
 import {Register} from "../Pages/Register.jsx";
 import {Home} from "../Pages/Home.jsx";
 import {NotFound} from "../Pages/NotFound.jsx";
+import {Layout} from "../Layout/Layout.jsx";
 
 export const routes = createBrowserRouter([
     {
-        path: "/login",
-        element: <Login/>
-    },
-    {
-        path: "/register",
-        element: <Register/>
-    },
-    {
-        path: "/",
-        element: <Home/>
-    },
-    {
-        path: "/*",
-        element: <NotFound/>
+        element: <Layout/>,
+        children: [
+            {
+                path: "/login",
+                element: <Login/>
+            },
+            {
+                path: "/register",
+                element: <Register/>
+            },
+            {
+                path: "/",
+                element: <Home/>
+            },
+            {
+                path: "/*",
+                element: <NotFound/>
+            }
+        ]
     }
-
 ]);
